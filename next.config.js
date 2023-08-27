e2d0adb725e1e5f8a7bd89bf6de787f31bb50064
alirezazahiri <itsalireza.zr@gmail.com>
2023-08-27 12:05:59 +0330

    constants updated

--- next.config.js
+++ next.config.js
@@ -5,7 +5,6 @@ const nextConfig = {
   env: {
     NEXT_APP_DB_URI: process.env.DB_URI,
     JWT_SECRET: process.env.JWT_SECRET,
-    API_BASE_URL: process.env.API_BASE_URL,
     DATABASE_URL: process.env.DATABASE_URL,
     KAVE_NEGAR_API_KEY: process.env.KAVE_NEGAR_API_KEY
   },
